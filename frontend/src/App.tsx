da0cbcc9575a5fb2559953c5d3a2d225f39c365f
Fernando Zavalia <zavaliafernando@gmail.com>
2019-12-25 18:27:28 -0300

    Join Game and Start Game views

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App: React.FC = () => {
-  const socket = io("http://localhost:8080");
+const socket = io("http://localhost:8080");
 
-  socket.on("connect", () => {
-    socket.emit("start-game");
-  });
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:gameId" component={JoinGame} />
+        <Route component={StartGame} />
+      </Switch>
+    </Router>
+  );
+};
 
-  socket.on("game-started", console.log);
+const StartGame = () => {
+  const [selectedHand, setSelectedHand] = useState<string | undefined>();
+
+  useEffect(() => {
+    const onGameStarted = (game: any) => {
+      console.log(game);
+      socket.emit("play-hand", selectedHand);
+    };
+    socket.on("game-started", onGameStarted);
+    return () => {
+      socket.off("game-started", onGameStarted);
+    };
+  }, [selectedHand]);
+
+  const onClick = (hand: string) => {
+    if (!selectedHand) {
+      socket.emit("start-game");
+      setSelectedHand(hand);
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button disabled={!!selectedHand} onClick={() => onClick("rock")}>
+        ROCK
+      </button>
+      <button disabled={!!selectedHand} onClick={() => onClick("paper")}>
+        PAPER
+      </button>
+      <button disabled={!!selectedHand} onClick={() => onClick("scissors")}>
+        SCISSORS
+      </button>
+      {selectedHand && <div>Selected: {selectedHand}</div>}
+    </div>
+  );
+};
+
+const JoinGame = (props: { gameId: string }) => {
+  const [selectedHand, setSelectedHand] = useState<string | undefined>();
+
+  useEffect(() => {}, [selectedHand]);
+
+  const onClick = (hand: string) => {
+    if (!selectedHand) {
+      setSelectedHand(hand);
+      socket.emit("play-hand", hand);
+    }
+  };
+
+  return (
+    <div>
+      <button disabled={!!selectedHand} onClick={() => onClick("rock")}>
+        ROCK
+      </button>
+      <button disabled={!!selectedHand} onClick={() => onClick("paper")}>
+        PAPER
+      </button>
+      <button disabled={!!selectedHand} onClick={() => onClick("scissors")}>
+        SCISSORS
+      </button>
+      {selectedHand && <div>Selected: {selectedHand}</div>}
     </div>
   );
 };
