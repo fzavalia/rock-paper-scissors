2e3c38a711ea562e7196b23d034ef13998bd78a0
Fernando Zavalia <fzavalia@mulesoft.com>
2019-12-27 20:57:47 -0300

    Refactor

--- backend/src/Connection.ts
+++ backend/src/Connection.ts
@@ -5,23 +5,30 @@ import Lobby from "./model/Lobby";
 import uuid from "uuid/v4";
 import Hand from "./model/Hand";
 
-export default class Handlers {
+export default class Connection {
   constructor(
+    private readonly socket: Socket,
     private readonly sockets: Map<string, Socket>,
     private readonly games: Map<string, Game>,
     private readonly lobbies: Map<string, Lobby>
-  ) {}
+  ) {
+    socket.on(events.CREATE_LOBBY, bestOf => this.createLobby(bestOf));
+    socket.on(events.JOIN_LOBBY, lobbyId => this.joinLobby(lobbyId));
+    socket.on(events.CREATE_GAME, lobbyId => this.createGame(lobbyId));
+    socket.on(events.PLAY_HAND, data => this.playHand(data.gameId, data.hand));
+    socket.on(events.DISCONNECT, () => this.disconnect());
+  }
 
-  createLobby = (socket: Socket, bestOf: number) => {
+  createLobby = (bestOf: number) => {
     const lobby: Lobby = new Lobby(uuid(), bestOf);
     this.lobbies.set(lobby.id, lobby);
-    socket.emit(events.CREATED_LOBBY, lobby);
+    this.socket.emit(events.CREATED_LOBBY, lobby);
   };
 
-  joinLobby = (socket: Socket, lobbyId: string) => {
+  joinLobby = (lobbyId: string) => {
     const lobby = this.getLobby(lobbyId);
-    lobby.join(socket.id);
-    this.emitToLobbyPlayers(lobby, events.JOINED_LOBBY, { lobby, playerId: socket.id });
+    lobby.join(this.socket.id);
+    this.emitToLobbyPlayers(lobby, events.JOINED_LOBBY, { lobby, playerId: this.socket.id });
   };
 
   createGame = (lobbyId: string) => {
@@ -31,16 +38,16 @@ export default class Handlers {
     this.emitToGamePlayers(game, events.CREATED_GAME, { game });
   };
 
-  disconnect = (socket: Socket) => {
-    this.sockets.delete(socket.id);
-    this.handleLobbyOnDisconnect(socket);
-    this.handleGameOnDisconnect(socket);
+  disconnect = () => {
+    this.sockets.delete(this.socket.id);
+    this.handleLobbyOnDisconnect();
+    this.handleGameOnDisconnect();
   };
 
-  playHand = (socket: Socket, gameId: string, hand: string) => {
+  playHand = (gameId: string, hand: string) => {
     const game = this.getGame(gameId);
-    game.playHand(socket.id, Hand.fromString(hand));
-    this.emitToGamePlayers(game, events.PLAYED_HAND, { game, playerId: socket.id });
+    game.playHand(this.socket.id, Hand.fromString(hand));
+    this.emitToGamePlayers(game, events.PLAYED_HAND, { game, playerId: this.socket.id });
     if (game.isRoundOver()) {
       this.emitToGamePlayers(game, events.ROUND_FINISHED, { game, winner: game.getRoundWinner() });
       if (!game.isOver()) {
@@ -50,15 +57,6 @@ export default class Handlers {
         this.games.delete(game.id);
       }
     }
-  };
-
-  connection = (socket: Socket) => {
-    this.sockets.set(socket.id, socket);
-    socket.on(events.CREATE_LOBBY, bestOf => this.createLobby(socket, bestOf));
-    socket.on(events.JOIN_LOBBY, lobbyId => this.joinLobby(socket, lobbyId));
-    socket.on(events.CREATE_GAME, lobbyId => this.createGame(lobbyId));
-    socket.on(events.PLAY_HAND, data => this.playHand(socket, data.gameId, data.hand));
-    socket.on(events.DISCONNECT, () => this.disconnect(socket));
   };
 
   private getLobby = (lobbyId: string): Lobby => {
@@ -77,33 +75,33 @@ export default class Handlers {
     return game;
   };
 
-  private handleLobbyOnDisconnect = (socket: Socket) => {
+  private handleLobbyOnDisconnect = () => {
     let lobby: Lobby | undefined;
     for (let l of Array.from(this.lobbies.values())) {
-      if (l.hasPlayer(socket.id)) {
+      if (l.hasPlayer(this.socket.id)) {
         lobby = l;
         break;
       }
     }
     if (lobby) {
-      lobby.remove(socket.id);
+      lobby.remove(this.socket.id);
       if (lobby.isEmpty()) {
         this.lobbies.delete(lobby.id);
       } else {
         this.emitToLobbyPlayers(
           lobby,
           events.JOINED_LOBBY,
-          { lobby, playerId: socket.id },
-          playerId => playerId !== socket.id
+          { lobby, playerId: this.socket.id },
+          playerId => playerId !== this.socket.id
         );
       }
     }
   };
 
-  private handleGameOnDisconnect = (socket: Socket) => {
+  private handleGameOnDisconnect = () => {
     let game: Game | undefined;
     for (let g of Array.from(this.games.values())) {
-      if (g.hasPlayer(socket.id)) {
+      if (g.hasPlayer(this.socket.id)) {
         game = g;
         break;
       }
@@ -113,8 +111,8 @@ export default class Handlers {
       this.emitToGamePlayers(
         game,
         events.OPPONENT_DISCONNECTED,
-        { game, playerId: socket.id },
-        playerId => playerId !== socket.id
+        { game, playerId: this.socket.id },
+        playerId => playerId !== this.socket.id
       );
     }
   };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,20 @@
-import io from "socket.io";
+import io, { Socket } from "socket.io";
 import express from "express";
 import http from "http";
 import * as events from "./events";
-import Handlers from "./handlers";
+import Connection from "./Connection";
+import Game from "./model/Game";
+import Lobby from "./model/Lobby";
 
 const app = express();
 const server = http.createServer(app);
 const wss = io(server);
 
-const handlers = new Handlers(new Map(), new Map(), new Map());
+const sockets = new Map<string, Socket>();
+const games = new Map<string, Game>();
+const lobbies = new Map<string, Lobby>();
 
-wss.on(events.CONNECTION, handlers.connection);
+wss.on(events.CONNECTION, socket => new Connection(socket, sockets, games, lobbies));
 
 server.listen(8080, function() {
   console.log("listening on *:8080");
