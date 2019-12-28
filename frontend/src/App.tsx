6a71ca6836f962c4b7bdf1c4a42d2c2286a4b2b4
Fernando Zavalia <zavaliafernando@gmail.com>
2019-12-28 12:52:07 -0300

    Test front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,81 +1,31 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const socket = io("http://localhost:8080");
 
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/:gameId" component={JoinGame} />
-        <Route component={StartGame} />
-      </Switch>
-    </Router>
-  );
-};
-
-const StartGame = () => {
-  const [selectedHand, setSelectedHand] = useState<string | undefined>();
-
+  const [lobbyId, setLobbyId] = useState<any>("");
   useEffect(() => {
-    const onGameStarted = (game: any) => {
-      console.log(game);
-      socket.emit("play-hand", selectedHand);
-    };
-    socket.on("game-started", onGameStarted);
-    return () => {
-      socket.off("game-started", onGameStarted);
-    };
-  }, [selectedHand]);
-
-  const onClick = (hand: string) => {
-    if (!selectedHand) {
-      socket.emit("start-game");
-      setSelectedHand(hand);
-    }
-  };
-
+    socket.on("created-lobby", (data: any) => {
+      console.log(data);
+      setLobbyId(data.lobby.id);
+    });
+    socket.on("joined-lobby", (data: any) => {
+      console.log(data);
+    });
+    socket.on("created-game", (data: any) => {
+      console.log(data);
+    });
+  }, []);
   return (
-    <div>
-      <button disabled={!!selectedHand} onClick={() => onClick("rock")}>
-        ROCK
-      </button>
-      <button disabled={!!selectedHand} onClick={() => onClick("paper")}>
-        PAPER
-      </button>
-      <button disabled={!!selectedHand} onClick={() => onClick("scissors")}>
-        SCISSORS
-      </button>
-      {selectedHand && <div>Selected: {selectedHand}</div>}
-    </div>
-  );
-};
-
-const JoinGame = (props: { gameId: string }) => {
-  const [selectedHand, setSelectedHand] = useState<string | undefined>();
-
-  useEffect(() => {}, [selectedHand]);
-
-  const onClick = (hand: string) => {
-    if (!selectedHand) {
-      setSelectedHand(hand);
-      socket.emit("play-hand", hand);
-    }
-  };
-
-  return (
-    <div>
-      <button disabled={!!selectedHand} onClick={() => onClick("rock")}>
-        ROCK
-      </button>
-      <button disabled={!!selectedHand} onClick={() => onClick("paper")}>
-        PAPER
-      </button>
-      <button disabled={!!selectedHand} onClick={() => onClick("scissors")}>
-        SCISSORS
-      </button>
-      {selectedHand && <div>Selected: {selectedHand}</div>}
+    <div style={{ display: "flex", flexDirection: "column", width: 500 }}>
+      <div style={{ display: "flex" }}>
+        <span style={{ marginRight: "1rem" }}>lobbyId</span>
+        <input style={{ flex: 1 }} value={lobbyId} onChange={e => setLobbyId(e.target.value)} />
+      </div>
+      <button onClick={() => socket.emit("create-lobby", 3)}>CREATE LOBBY</button>
+      <button onClick={() => socket.emit("join-lobby", lobbyId)}>JOIN LOBBY</button>
+      <button onClick={() => socket.emit("create-game", lobbyId)}>CREATE GAME</button>
     </div>
   );
 };
