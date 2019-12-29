a3e2e373a11d4575bb024733db471d3c9356ff10
Fernando Zavalia <zavaliafernando@gmail.com>
2019-12-29 17:27:21 -0300

    Refactor

--- backend/src/model/Round.spec.ts
+++ backend/src/model/Round.spec.ts
@@ -4,21 +4,23 @@ import Hand, { HandType } from "./Hand";
 const player1Id = "player1Id";
 const player2Id = "player2Id";
 
+const makeRound = () => new Round(player1Id, player2Id);
+
 describe("Round", () => {
   describe("playHand", () => {
     it("succeeds", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       round.playHand(player1Id, new Hand(HandType.ROCK));
       round.playHand(player2Id, new Hand(HandType.ROCK));
     });
 
     it("fails when the user does not belong to the round", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       expect(() => round.playHand("invalid", new Hand(HandType.ROCK))).toThrow();
     });
 
     it("fails when a player has already played", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       round.playHand(player1Id, new Hand(HandType.ROCK));
       expect(() => round.playHand(player1Id, new Hand(HandType.ROCK))).toThrow();
     });
@@ -26,7 +28,7 @@ describe("Round", () => {
 
   describe("isOver", () => {
     it("returns true when both players have played", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       expect(round.isOver()).toBeFalsy();
       round.playHand(player1Id, new Hand(HandType.ROCK));
       expect(round.isOver()).toBeFalsy();
@@ -37,14 +39,14 @@ describe("Round", () => {
 
   describe("getWinner", () => {
     it("returns undefined on tie", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       round.playHand(player1Id, new Hand(HandType.ROCK));
       round.playHand(player2Id, new Hand(HandType.ROCK));
       expect(round.getWinner()).toBeUndefined();
     });
 
     it("returns the winning player when not tie", () => {
-      const round = new Round(player1Id, player2Id);
+      const round = makeRound();
       round.playHand(player1Id, new Hand(HandType.PAPER));
       round.playHand(player2Id, new Hand(HandType.ROCK));
       expect(round.getWinner()).toBe(player1Id);
