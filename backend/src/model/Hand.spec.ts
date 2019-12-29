9fcb3c15a4320b8b284f3e6633e0d068b1a8a2b2
Fernando Zavalia <zavaliafernando@gmail.com>
2019-12-29 13:06:55 -0300

    Full coverage on Hand

---
+++ backend/src/model/Hand.spec.ts
@@ -0,0 +1,34 @@
+import Hand, { HandType, HandComparison } from "./Hand";
+
+const expectResult = (handType1: HandType, handType2: HandType, result: HandComparison) => {
+  const hand1 = new Hand(handType1);
+  const hand2 = new Hand(handType2);
+  expect(hand1.testAgainst(hand2)).toBe(result);
+};
+
+const expectWin = (handType1: HandType, handType2: HandType) => expectResult(handType1, handType2, HandComparison.WIN);
+
+const expectFromString = (str: string, handType: HandType) => expect(Hand.fromString(str).value).toBe(handType);
+
+describe("Hand", () => {
+  describe("testAgainst", () => {
+    it("paper wins against rock", () => expectWin(HandType.PAPER, HandType.ROCK));
+    it("scissors win against paper", () => expectWin(HandType.SCISSORS, HandType.PAPER));
+    it("rock wins against scissors", () => expectWin(HandType.ROCK, HandType.SCISSORS));
+    it("win result", () => expectResult(HandType.SCISSORS, HandType.PAPER, HandComparison.WIN));
+    it("tie result", () => expectResult(HandType.SCISSORS, HandType.SCISSORS, HandComparison.TIE));
+    it("lose result", () => expectResult(HandType.SCISSORS, HandType.ROCK, HandComparison.LOSE));
+  });
+
+  describe("fromString", () => {
+    it("succeeds", () => {
+      expectFromString("rock", HandType.ROCK);
+      expectFromString("scissors", HandType.SCISSORS);
+      expectFromString("paper", HandType.PAPER);
+    });
+
+    it("fails on wrong input", () => {
+      expect(() => Hand.fromString("invalid")).toThrow();
+    });
+  });
+});
