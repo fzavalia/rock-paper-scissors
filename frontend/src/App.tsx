e865ce8206517cebbc4248be754bea6d34041dcc
Fernando Zavalia <fzavalia@mulesoft.com>
2020-01-02 23:01:42 -0300

    Game component base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,25 @@ const App = () => (
   <Router>
     <Switch>
       <Route path="/lobbies/:id" render={props => <Lobby id={props.match.params.id} />}></Route>
-      <Route path="/games/:id" render={() => <div>Game</div>}></Route>
+      <Route path="/games/:id" render={props => <Game id={props.match.params.id} />}></Route>
       <Route component={CreateLobby}></Route>
     </Switch>
   </Router>
 );
+
+const Game = (props: { id: string }) => {
+  useEffect(() => {
+    socket.emit("find-game", props.id);
+  }, []);
+
+  useEffect(() => {
+    socket.on("found-game", console.log);
+    return () => {
+      socket.off("found-game", console.log);
+    };
+  });
+
+  return null;
+};
 
 export default App;
