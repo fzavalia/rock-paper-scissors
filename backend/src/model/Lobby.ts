a36ad40d533ba505092da46d0e553bd7e681dbea
Fernando Zavalia <zavaliafernando@gmail.com>
2020-01-07 00:02:57 -0300

    Update lobby

--- backend/src/model/Lobby.ts
+++ backend/src/model/Lobby.ts
@@ -2,33 +2,55 @@ import Game from "./Game";
 import HasPlayers from "./interfaces/HasPlayers";
 
 export default class Lobby implements HasPlayers {
-  private readonly playerIds = new Set<string>();
+  private player1Id?: string;
+  private player2Id?: string;
 
   constructor(readonly id: string, readonly bestOf: number) {}
 
-  getPlayerIds = () => Array.from(this.playerIds);
+  getPlayerIds = () => {
+    const playerIds: string[] = [];
+    if (this.player1Id) {
+      playerIds.push(this.player1Id);
+    }
+    if (this.player2Id) {
+      playerIds.push(this.player2Id);
+    }
+    return playerIds;
+  };
 
-  hasPlayer = (playerId: string) => this.playerIds.has(playerId);
+  hasPlayer = (playerId: string) => this.getPlayerIds().includes(playerId);
 
   join = (playerId: string) => {
-    if (this.playerIds.has(playerId)) {
+    if (this.getPlayerIds().includes(playerId)) {
       return;
     }
-    if (this.playerIds.size >= 2) {
+    if (this.getPlayerIds().length >= 2) {
       throw new Error("Lobby is full");
     }
-    this.playerIds.add(playerId);
+    if (!this.player1Id) {
+      this.player1Id = playerId;
+    } else {
+      this.player2Id = playerId;
+    }
   };
 
   toGame = () => {
-    if (this.playerIds.size < 2) {
+    const player1Id = this.player1Id;
+    const player2Id = this.player2Id;
+    if (!player1Id || !player2Id) {
       throw new Error("Missing players");
     }
-    const playerIds = this.playerIds.values();
-    return new Game(this.id, this.bestOf, playerIds.next().value, playerIds.next().value);
+    return new Game(this.id, this.bestOf, player1Id, player2Id);
   };
 
-  remove = (playerId: string) => this.playerIds.delete(playerId);
+  remove = (playerId: string) => {
+    if (this.player1Id === playerId) {
+      this.player1Id = undefined;
+    }
+    if (this.player2Id === playerId) {
+      this.player2Id = undefined;
+    }
+  };
 
   isEmpty = () => this.getPlayerIds().length === 0;
 }
