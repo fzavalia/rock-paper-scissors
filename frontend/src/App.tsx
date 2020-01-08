d39a1bed5ab1cb5ba2a459904cd6ef80cfbacdc7
Fernando Zavalia <zavaliafernando@gmail.com>
2020-01-08 02:48:46 -0300

    Game flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,51 +7,115 @@ import { Typography, Button } from "@material-ui/core";
 
 export const socket = io("http://localhost:8080");
 
-// const App = () => (
-//   <Router>
-//     <Switch>
-//       <Route path="/lobbies/:id" render={props => <Lobby id={props.match.params.id} />}></Route>
-//       <Route path="/games/:id" render={props => <Game id={props.match.params.id} />}></Route>
-//       <Route component={CreateLobby}></Route>
-//     </Switch>
-//   </Router>
-// );
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/lobbies/:id" render={props => <Lobby id={props.match.params.id} />}></Route>
+      <Route path="/games/:id" render={props => <Game id={props.match.params.id} />}></Route>
+      <Route component={CreateLobby}></Route>
+    </Switch>
+  </Router>
+);
 
-const App = () => <Game id={"gameId"} />;
+// const App = () => <Game id={"gameId"} />;
 
 const Game = (props: { id: string }) => {
-  const [game, setGame] = useState<any>({
-    id: props.id,
-    bestOf: 3,
-    player1Score: 0,
-    player2Score: 1,
-    isOver: true
-  });
+  const history = useHistory();
+
+  const [game, setGame] = useState<any>(undefined);
   const [playedHand, setPlayedHand] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    socket.emit("join-game", props.id);
+  }, []);
+
+  useEffect(() => {
+    socket.on("updated-game", setGame);
+    socket.on("runtime-error", console.log);
+    return () => {
+      socket.off("updated-game", setGame);
+      socket.off("runtime-error", console.log);
+    };
+  });
 
   if (!game) {
     return null;
   }
 
-  const opponentHasToPlay = () => true;
-
-  const alreadyPlayedHand = () => true;
-
   return (
     <div style={{ padding: "1rem" }}>
-      <Typography variant="h3" gutterBottom>
-        Game
+      <Typography variant="body1" gutterBottom>
+        Goal: <b>{game.goal}</b>
       </Typography>
+      <Typography variant="body1" gutterBottom>
+        Your score: <b>{game.playerScore}</b> - Opponent score: <b>{game.opponentScore}</b>
+      </Typography>
+      {!game.isOver ? (
+        <>
+          {game.hasToPlay ? (
+            <Typography variant="body1" gutterBottom>
+              Select what you want to play by clicking on one of the buttons...
+            </Typography>
+          ) : (
+            <Typography variant="body1" gutterBottom>
+              You have chosen <b>{playedHand}</b>! Waiting for opponent to play...
+            </Typography>
+          )}
+          <div>
+            <Button
+              disabled={!game.hasToPlay}
+              style={{ marginRight: "1rem" }}
+              variant="outlined"
+              onClick={() => {
+                setPlayedHand("Rock");
+                socket.emit("play-hand", { gameId: game.id, hand: "rock" });
+              }}
+            >
+              ROCK
+            </Button>
+            <Button
+              disabled={!game.hasToPlay}
+              style={{ marginRight: "1rem" }}
+              variant="outlined"
+              onClick={() => {
+                setPlayedHand("Paper");
+                socket.emit("play-hand", { gameId: game.id, hand: "paper" });
+              }}
+            >
+              PAPER
+            </Button>
+            <Button
+              disabled={!game.hasToPlay}
+              style={{ marginRight: "1rem" }}
+              variant="outlined"
+              onClick={() => {
+                setPlayedHand("Scissors");
+                socket.emit("play-hand", { gameId: game.id, hand: "scissors" });
+              }}
+            >
+              SCISSORS
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          {game.winner === socket.id ? (
+            <Typography variant="body1" gutterBottom>
+              You have <b>won</b> the game!
+            </Typography>
+          ) : (
+            <Typography variant="body1" gutterBottom>
+              You have <b>lost</b> the game...
+            </Typography>
+          )}
 
-      <div style={{ marginBottom: "1rem" }}>
-        Best Of: <b>{game.bestOf}</b>
-      </div>
+          <Button variant="outlined" onClick={() => history.push("/")}>
+            Return
+          </Button>
+        </>
+      )}
 
-      <div style={{ marginBottom: "1rem" }}>
-        Your score: <b>{game.player1Score}</b> - Opponent score: <b>{game.player2Score}</b>
-      </div>
-
-      {game.isOver ? (
+      {/* {game.isOver ? (
         <>
           <div style={{ marginBottom: "1rem" }}>Game Over</div>
           <div style={{ marginBottom: "1rem" }}>
@@ -77,24 +141,13 @@ const Game = (props: { id: string }) => {
               <span>Your opponent is waiting for you to play!</span>
             )}
           </div>
-
           <div style={{ marginBottom: "1rem" }}>
             {alreadyPlayedHand() ? <span>You have chosen {playedHand}</span> : <span>Choose a hand to play</span>}
           </div>
 
-          <div>
-            <Button disabled={alreadyPlayedHand()} style={{ marginRight: "1rem" }} variant="outlined">
-              ROCK
-            </Button>
-            <Button disabled={alreadyPlayedHand()} style={{ marginRight: "1rem" }} variant="outlined">
-              PAPER
-            </Button>
-            <Button disabled={alreadyPlayedHand()} style={{ marginRight: "1rem" }} variant="outlined">
-              SCISSORS
-            </Button>
-          </div>
+          
         </>
-      )}
+      )} */}
     </div>
   );
 };
