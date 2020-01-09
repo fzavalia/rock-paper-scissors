80cd5e0f47e103baef5d8e43f0a16f695983f4f4
Fernando Zavalia <zavaliafernando@gmail.com>
2020-01-08 21:49:40 -0300

    Rounds feed

--- frontend/src/views/Game.tsx
+++ frontend/src/views/Game.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button, Card, CardContent, List, ListItem } from "@material-ui/core";
 import { socket } from "../App";
 
 const Game = (props: { id: string }) => {
@@ -53,14 +53,41 @@ const Game = (props: { id: string }) => {
         </>
       ) : (
         <>
-          <Typography variant="body1" gutterBottom>
-            You have <b>{game.winner ? "won" : "lost"}</b> the game!
+          <Typography variant="body2" gutterBottom>
+            You have <b>{game.winner ? "WON" : "LOST"}</b> the game!
           </Typography>
           <Button variant="outlined" onClick={() => history.push("/")}>
             Return
           </Button>
         </>
       )}
+      <Card style={{ marginTop: "1rem" }}>
+        <CardContent>
+          <Typography variant="caption" gutterBottom>
+            Feed
+          </Typography>
+          <List>
+            {game.rounds
+              .filter((r: any) => r.isOver)
+              .map((r: any) => (
+                <ListItem>
+                  <Typography variant="body1">
+                    {r.playerHand === r.opponentHand ? (
+                      <>
+                        <i>TIED</i> with <b>{r.playerHand.toUpperCase()}</b>
+                      </>
+                    ) : (
+                      <>
+                        <i>{r.winner ? "WIN" : "LOSE"}</i> with <b>{r.playerHand.toUpperCase()}</b> against{" "}
+                        <b>{r.opponentHand.toUpperCase()}</b>
+                      </>
+                    )}
+                  </Typography>
+                </ListItem>
+              ))}
+          </List>
+        </CardContent>
+      </Card>
     </div>
   );
 };
