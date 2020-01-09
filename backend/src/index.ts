59e64901de4d4fcf880c3da5031e0ce4bcc8193c
Fernando Zavalia <zavaliafernando@gmail.com>
2020-01-09 20:55:20 -0300

    Refactor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,29 +14,22 @@ const sockets = new Map<string, Socket>();
 const games = new Map<string, Game>();
 const lobbies = new Map<string, Lobby>();
 
-const tenMinutes = 10 * 60 * 1000;
+const scheduleStaleElementsRemoval = (map: Map<string, { lastInteraction: Date }>) => {
+  const tenMinutes = 10 * 60 * 1000;
+  setInterval(() => {
+    console.log("Deleting stale elements...");
+    const now = +new Date();
+    Array.from(map)
+      .filter(([_, element]) => now - +element.lastInteraction > tenMinutes)
+      .forEach(([id, _]) => {
+        map.delete(id);
+        console.log(`Deleted element ${id}!`);
+      });
+  }, tenMinutes);
+};
 
-setInterval(() => {
-  console.log("Deleting stale games...");
-  const now = +new Date();
-  Array.from(games)
-    .filter(([_, game]) => now - +game.lastInteraction > tenMinutes)
-    .forEach(([id, _]) => {
-      games.delete(id);
-      console.log(`Deleted game ${id}!`);
-    });
-}, tenMinutes);
-
-setInterval(() => {
-  console.log("Deleting stale lobbies...");
-  const now = +new Date();
-  Array.from(lobbies)
-    .filter(([_, lobby]) => now - +lobby.lastInteraction > tenMinutes)
-    .forEach(([id, _]) => {
-      lobbies.delete(id);
-      console.log(`Deleted lobby ${id}!`);
-    });
-}, tenMinutes);
+scheduleStaleElementsRemoval(games);
+scheduleStaleElementsRemoval(lobbies);
 
 wss.on(events.CONNECTION, socket => new Connection(socket, sockets, games, lobbies));
 
