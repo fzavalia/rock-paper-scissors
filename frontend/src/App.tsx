a78a44e9d053cc1325b4109976ce3c31e96ebb50
Fernando Zavalia <zavaliafernando@gmail.com>
2020-01-11 20:11:09 -0300

    Change styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,17 +14,45 @@ const App = () => {
   return (
     <SnackbarProvider maxSnack={3}>
       <ErrorNotification>
-        <div style={{ padding: "1rem" }}>
-          <Typography variant="h5" gutterBottom>
-            <b>Rock, Paper Scissors Online!</b>
-          </Typography>
-          <Router>
-            <Switch>
-              <Route path="/lobbies/:id" render={props => <Lobby id={props.match.params.id} />}></Route>
-              <Route path="/games/:id" render={props => <Game id={props.match.params.id} />}></Route>
-              <Route component={CreateLobby}></Route>
-            </Switch>
-          </Router>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <div style={{ textAlign: "center", maxWidth: 400, width: "100%", margin: "1rem" }}>
+            <Typography
+              style={{
+                color: "white",
+                width: "100%",
+                textShadow: "0px 0px 5px rgba(0, 0, 0, 0.14)",
+                marginBottom: "2rem",
+                marginTop: "1rem"
+              }}
+              variant="h5"
+              gutterBottom
+            >
+              <b>Rock, Paper Scissors Online!</b>
+            </Typography>
+            <div
+              style={{
+                boxSizing: "border-box",
+                padding: "1rem",
+                width: "100%",
+                backgroundColor: "white",
+                borderRadius: "1rem",
+                boxShadow: "0px 0px 20px 10px rgba(0,0,0,0.14)"
+              }}
+            >
+              <Router>
+                <Switch>
+                  <Route path="/lobbies/:id" render={props => <Lobby id={props.match.params.id} />}></Route>
+                  <Route path="/games/:id" render={props => <Game id={props.match.params.id} />}></Route>
+                  <Route component={CreateLobby}></Route>
+                </Switch>
+              </Router>
+            </div>
+          </div>
         </div>
       </ErrorNotification>
     </SnackbarProvider>
